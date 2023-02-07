d6c02c032e9ec7e10930227fe8b0a22f0a5f8b52
manuelernesto <alemdadox@gmail.com>
2023-02-07 23:13:26 +0100

    init setup

---
+++ src/main/kotlin/io/github/manuelernesto/onlinejavasummitdemo/OnlineJavaSummitDemoApplication.kt
@@ -0,0 +1,11 @@
+package io.github.manuelernesto.onlinejavasummitdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineJavaSummitDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<OnlineJavaSummitDemoApplication>(*args)
+}
